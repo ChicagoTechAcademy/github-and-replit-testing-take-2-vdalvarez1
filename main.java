ce6e3804688047a96b7383de2fe15f4a6b47ede4
Vicente Alvarez <valvarez@chicagotechacademy.org>
2020-10-05 18:21:59 +0000

    submit

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ public class main{
     public static void main(String[] args)
       {
           //write your name where it says name.
-        System.out.println("NAME"); 
+        System.out.println("Vicente"); 
       }
     }
     
